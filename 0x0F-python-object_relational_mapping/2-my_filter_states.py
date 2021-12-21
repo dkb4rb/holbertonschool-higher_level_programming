be4d3d3e7d9f1714e2dc47d62268bad61a6390af
root <root@localhost.localdomain>
2021-12-20 23:12:36 -0500

    Advanced in the task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Create ne function to conect MySQLdb
+"""
+
+import sys
+import MySQLdb
+
+
+def new_process():
+    """ Values for the connect to the database """
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    """Stattics values to nc connection"""
+    ht = "localhost"
+    pt = 3306
+
+    """Connecting to the database"""
+    d_b = MySQLdb.connect(host=ht, user=usr, password=pwd, db=db, port=pt)
+
+    """The cursor gives us the ability to have multiple seperate
+    working environments through the same connection to the database"""
+    cur = d_b.cursor()
+
+    """Executing the query"""
+    cur.execute("SELECT * \
+    FROM `states` \
+    WHERE BINARY name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+
+    """ Show the results find """
+    [print(state) for state in cur.fetchall()]
+
+    cur.close()
+
+    """Closing the connection to the database"""
+    d_b.close()
+
+
+if __name__ == "__main__":
+    new_process()
