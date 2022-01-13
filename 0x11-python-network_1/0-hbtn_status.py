4cfacd6c1d4f4dc9586bb88dc9dabeb78dddcced
root <root@localhost.localdomain>
2022-01-12 21:09:47 -0500

    Requests headers to holberton url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,12 +10,12 @@ url = 'https://intranet.hbtn.io/status'
 def new_request():
     """ Representation request header function"""
     reqt = request.Request(url)
-    response = request.urlopen(reqt)
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    with request.urlopen(reqt) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
 
 """ Declare the flush to program """
