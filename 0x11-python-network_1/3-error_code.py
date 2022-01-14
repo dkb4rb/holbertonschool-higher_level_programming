f91614b4d67f9b3ac3d42000b7e8192eec487de1
root <root@localhost.localdomain>
2022-01-13 19:04:53 -0500

    Requests headers to holberton url

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,15 +8,10 @@ import urllib.error
 import urllib.request
 
 
-def new_request():
-    url = sys.argv[1]
-    request = urllib.request.Request(url, data)
-    try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
-
-if __name__ == "__main__":
-    new_request()
+url = sys.argv[1]
+request = urllib.request.Request(url, data)
+try:
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("ascii"))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
