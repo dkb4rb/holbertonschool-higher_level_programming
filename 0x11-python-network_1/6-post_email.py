13fc96f69a10d2a2811e559e66ea5437fef7f8e1
root <root@localhost.localdomain>
2022-01-13 19:14:09 -0500

    Requests headers to holberton url, email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Import libraries
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
